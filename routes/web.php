e30c54523c5dacf06ca1c7ddee2e0c55f75a7857
unknown <waqar95>
2019-10-22 20:56:33 +0500

    second commit created About and Contact page

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/about', function () {
+    return view('about');
 });
