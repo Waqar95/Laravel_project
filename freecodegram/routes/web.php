a1f0c618f24b2c063d74e76340c442390827010a
unknown <waqar95>
2019-10-23 20:46:06 +0500

    added js,css,composer and auth folder

--- freecodegram/routes/web.php
+++ freecodegram/routes/web.php
@@ -14,18 +14,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-Route::get('/about', function () {
-    return view('about');
-});
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
